8c57dadc0022aea4ac25ddc928df8544cb71f045
CrissUD <cfpatinoc@correo.udistrital.edu.co>
2020-11-12 21:46:57 -0500

    feat: Se agrega llave foranea al modelo de subtipo_produccion_academica

--- models/subtipo_produccion.go
+++ models/subtipo_produccion.go
@@ -11,15 +11,16 @@ import (
 )
 
 type SubtipoProduccion struct {
-	Id                int             `orm:"column(id);pk;auto"`
-	Nombre            string          `orm:"column(nombre)"`
-	Descripcion       string          `orm:"column(descripcion);null"`
-	CodigoAbreviacion string          `orm:"column(codigo_abreviacion);null"`
-	Activo            bool            `orm:"column(activo)"`
-	NumeroOrden       float64         `orm:"column(numero_orden);null"`
-	FechaCreacion     time.Time       `orm:"column(fecha_creacion);type(timestamp without time zone)"`
-	FechaModificacion time.Time       `orm:"column(fecha_modificacion);type(timestamp without time zone)"`
-	TipoProduccionId  *TipoProduccion `orm:"column(tipo_produccion_id);rel(fk)"`
+	Id                  int                  `orm:"column(id);pk;auto"`
+	Nombre              string               `orm:"column(nombre)"`
+	Descripcion         string               `orm:"column(descripcion);null"`
+	CodigoAbreviacion   string               `orm:"column(codigo_abreviacion);null"`
+	Activo              bool                 `orm:"column(activo)"`
+	NumeroOrden         float64              `orm:"column(numero_orden);null"`
+	FechaCreacion       time.Time            `orm:"column(fecha_creacion);type(timestamp without time zone)"`
+	FechaModificacion   time.Time            `orm:"column(fecha_modificacion);type(timestamp without time zone)"`
+	TipoProduccionId    *TipoProduccion      `orm:"column(tipo_produccion_id);rel(fk)"`
+	CategoriaProduccion *CategoriaProduccion `orm:"column(categoria_produccion_id);rel(fk)"`
 }
 
 func (t *SubtipoProduccion) TableName() string {
