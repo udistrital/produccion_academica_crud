f22db532389e2d7a76e16d70d1d9ab14e2cd4269
Camilo <camiloquirogam@outlook.com>
2020-11-23 10:41:28 -0500

    feat: ajuste de controlador "categoria:_Produccion"

--- controllers/categoria_produccion.go
+++ controllers/categoria_produccion.go
@@ -13,13 +13,13 @@ import (
 	"github.com/astaxie/beego/logs"
 )
 
-// SistemaTipoProduccionController operations for SistemaTipoProduccion
-type SistemaTipoProduccionController struct {
+// CategoriaProduccionController operations for CategoriaProduccion
+type CategoriaProduccionController struct {
 	beego.Controller
 }
 
 // URLMapping ...
-func (c *SistemaTipoProduccionController) URLMapping() {
+func (c *CategoriaProduccionController) URLMapping() {
 	c.Mapping("Post", c.Post)
 	c.Mapping("GetOne", c.GetOne)
 	c.Mapping("GetAll", c.GetAll)
@@ -29,176 +29,173 @@ func (c *SistemaTipoProduccionController) URLMapping() {
 
 // Post ...
 // @Title Post
-// @Description create SistemaTipoProduccion
-// @Param	body		body 	models.SistemaTipoProduccion	true		"body for SistemaTipoProduccion content"
-// @Success 201 {int} models.SistemaTipoProduccion
+// @Description create CategoriaProduccion
+// @Param	body		body 	models.CategoriaProduccion	true		"body for CategoriaProduccion content"
+// @Success 201 {int} models.CategoriaProduccion
 // @Failure 400 the request contains incorrect syntax
 // @router / [post]
-func (c *SistemaTipoProduccionController) Post() {
+func (c *CategoriaProduccionController) Post() {
 	var v models.SistemaTipoProduccion
 	if err := json.Unmarshal(c.Ctx.Input.RequestBody, &v); err == nil {
-<<<<<<< HEAD:controllers/categoria_produccion.go
+
 		v.FechaCreacion = time_bogota.TiempoBogotaFormato()
 		v.FechaModificacion = time_bogota.TiempoBogotaFormato()
-		if _, err := models.AddSoporteProduccionAcademica(&v); err == nil {
-=======
-		if _, err := models.AddSistemaTipoProduccion(&v); err == nil {
->>>>>>> 639eb45fc4290940494fe1c6e8d0ae81d30b577f:controllers/sistema_tipo_produccion.go
-			c.Ctx.Output.SetStatus(201)
-			c.Data["json"] = v
+		
+
+			if _, err := models.AddCategoriaProduccion(&v); err == nil {
+
+				c.Ctx.Output.SetStatus(201)
+				c.Data["json"] = v
+			} else {
+				logs.Error(err)
+				//c.Data["development"] = map[string]interface{}{"Code": "000", "Body": err.Error(), "Type": "error"}
+				c.Data["system"] = err
+				c.Abort("400")
+			}
 		} else {
 			logs.Error(err)
 			//c.Data["development"] = map[string]interface{}{"Code": "000", "Body": err.Error(), "Type": "error"}
 			c.Data["system"] = err
 			c.Abort("400")
 		}
-	} else {
-		logs.Error(err)
-		//c.Data["development"] = map[string]interface{}{"Code": "000", "Body": err.Error(), "Type": "error"}
-		c.Data["system"] = err
-		c.Abort("400")
+		c.ServeJSON()
 	}
-	c.ServeJSON()
-}
 
 // GetOne ...
 // @Title Get One
-// @Description get SistemaTipoProduccion by id
+// @Description get CategoriaProduccion by id
 // @Param	id		path 	string	true		"The key for staticblock"
-// @Success 200 {object} models.SistemaTipoProduccion
+// @Success 200 {object} models.CategoriaProduccion
 // @Failure 404 not found resource
 // @router /:id [get]
-func (c *SistemaTipoProduccionController) GetOne() {
-	idStr := c.Ctx.Input.Param(":id")
-	id, _ := strconv.Atoi(idStr)
-	v, err := models.GetSistemaTipoProduccionById(id)
-	if err != nil {
-		logs.Error(err)
-		//c.Data["development"] = map[string]interface{}{"Code": "000", "Body": err.Error(), "Type": "error"}
-		c.Data["system"] = err
-		c.Abort("404")
-	} else {
-		c.Data["json"] = v
+	func(c *CategoriaProduccionController) GetOne() {
+	
+		idStr := c.Ctx.Input.Param(":id")
+		id, _ := strconv.Atoi(idStr)
+		v, err := models.GetCategoriaProduccionById(id)
+		if err != nil {
+			logs.Error(err)
+			//c.Data["development"] = map[string]interface{}{"Code": "000", "Body": err.Error(), "Type": "error"}
+			c.Data["system"] = err
+			c.Abort("404")
+		} else {
+			c.Data["json"] = v
+		}
+		c.ServeJSON()
 	}
-	c.ServeJSON()
-}
 
 // GetAll ...
 // @Title Get All
-// @Description get SistemaTipoProduccion
+// @Description get CategoriaProduccion
 // @Param	query	query	string	false	"Filter. e.g. col1:v1,col2:v2 ..."
 // @Param	fields	query	string	false	"Fields returned. e.g. col1,col2 ..."
 // @Param	sortby	query	string	false	"Sorted-by fields. e.g. col1,col2 ..."
 // @Param	order	query	string	false	"Order corresponding to each sortby field, if single value, apply to all sortby fields. e.g. desc,asc ..."
 // @Param	limit	query	string	false	"Limit the size of result set. Must be an integer"
 // @Param	offset	query	string	false	"Start position of result set. Must be an integer"
-// @Success 200 {object} models.SistemaTipoProduccion
+// @Success 200 {object} models.CategoriaProduccion
 // @Failure 404 not found resource
 // @router / [get]
-func (c *SistemaTipoProduccionController) GetAll() {
-	var fields []string
-	var sortby []string
-	var order []string
-	var query = make(map[string]string)
-	var limit int64 = 10
-	var offset int64
+	func(c *CategoriaProduccionController) GetAll()	{
+		var fields []string
+		var sortby []string
+		var order []string
+		var query = make(map[string]string)
+		var limit int64 = 10
+		var offset int64
 
-	// fields: col1,col2,entity.col3
-	if v := c.GetString("fields"); v != "" {
-		fields = strings.Split(v, ",")
-	}
-	// limit: 10 (default is 10)
-	if v, err := c.GetInt64("limit"); err == nil {
-		limit = v
-	}
-	// offset: 0 (default is 0)
-	if v, err := c.GetInt64("offset"); err == nil {
-		offset = v
-	}
-	// sortby: col1,col2
-	if v := c.GetString("sortby"); v != "" {
-		sortby = strings.Split(v, ",")
-	}
-	// order: desc,asc
-	if v := c.GetString("order"); v != "" {
-		order = strings.Split(v, ",")
-	}
-	// query: k:v,k:v
-	if v := c.GetString("query"); v != "" {
-		for _, cond := range strings.Split(v, ",") {
-			kv := strings.SplitN(cond, ":", 2)
-			if len(kv) != 2 {
-				c.Data["json"] = errors.New("Error: invalid query key/value pair")
-				c.ServeJSON()
-				return
+		// fields: col1,col2,entity.col3
+		if v := c.GetString("fields"); v != "" {
+			fields = strings.Split(v, ",")
+		}
+		// limit: 10 (default is 10)
+		if v, err := c.GetInt64("limit"); err == nil {
+			limit = v
+		}
+		// offset: 0 (default is 0)
+		if v, err := c.GetInt64("offset"); err == nil {
+			offset = v
+		}
+		// sortby: col1,col2
+		if v := c.GetString("sortby"); v != "" {
+			sortby = strings.Split(v, ",")
+		}
+		// order: desc,asc
+		if v := c.GetString("order"); v != "" {
+			order = strings.Split(v, ",")
+		}
+		// query: k:v,k:v
+		if v := c.GetString("query"); v != "" {
+			for _, cond := range strings.Split(v, ",") {
+				kv := strings.SplitN(cond, ":", 2)
+				if len(kv) != 2 {
+					c.Data["json"] = errors.New("Error: invalid query key/value pair")
+					c.ServeJSON()
+					return
+				}
+				k, v := kv[0], kv[1]
+				query[k] = v
 			}
-			k, v := kv[0], kv[1]
-			query[k] = v
 		}
-	}
 
-	l, err := models.GetAllSistemaTipoProduccion(query, fields, sortby, order, offset, limit)
-	if err != nil {
-		logs.Error(err)
-		//c.Data["development"] = map[string]interface{}{"Code": "000", "Body": err.Error(), "Type": "error"}
-		c.Data["system"] = err
-		c.Abort("404")
-	} else {
-		if l == nil {
-			l = append(l, map[string]interface{}{})
-		}
-		c.Data["json"] = l
-	}
-	c.ServeJSON()
-}
-
-// Put ...
-// @Title Put
-// @Description update the SistemaTipoProduccion
-// @Param	id		path 	string	true		"The id you want to update"
-// @Param	body		body 	models.SistemaTipoProduccion	true		"body for SistemaTipoProduccion content"
-// @Success 200 {object} models.SistemaTipoProduccion
-// @Failure 400 the request contains incorrect syntax
-// @router /:id [put]
-func (c *SistemaTipoProduccionController) Put() {
-	idStr := c.Ctx.Input.Param(":id")
-	id, _ := strconv.Atoi(idStr)
-	v := models.SistemaTipoProduccion{Id: id}
-	if err := json.Unmarshal(c.Ctx.Input.RequestBody, &v); err == nil {
-<<<<<<< HEAD:controllers/categoria_produccion.go
-		v.FechaCreacion = time_bogota.TiempoCorreccionFormato(v.FechaCreacion)
-		v.FechaModificacion = time_bogota.TiempoBogotaFormato()
-		if err := models.UpdateSoporteProduccionAcademicaById(&v); err == nil {
-=======
-		if err := models.UpdateSistemaTipoProduccionById(&v); err == nil {
->>>>>>> 639eb45fc4290940494fe1c6e8d0ae81d30b577f:controllers/sistema_tipo_produccion.go
-			c.Data["json"] = v
-		} else {
+		l, err := models.GetAllCategoriaProduccion(query, fields, sortby, order, offset, limit)
+		if err != nil {
 			logs.Error(err)
 			//c.Data["development"] = map[string]interface{}{"Code": "000", "Body": err.Error(), "Type": "error"}
 			c.Data["system"] = err
-			c.Abort("400")
+			c.Abort("404")
+		} else {
+			if l == nil {
+				l = append(l, map[string]interface{}{})
+			}
+			c.Data["json"] = l
 		}
-	} else {
-		logs.Error(err)
-		//c.Data["development"] = map[string]interface{}{"Code": "000", "Body": err.Error(), "Type": "error"}
-		c.Data["system"] = err
-		c.Abort("400")
+		c.ServeJSON()
 	}
-	c.ServeJSON()
-}
+
+// Put ...
+// @Title Put
+// @Description update the CategoriaProduccion
+// @Param	id		path 	string	true		"The id you want to update"
+// @Param	body		body 	models.CategoriaProduccion	true		"body for CategoriaProduccion content"
+// @Success 200 {object} models.CategoriaProduccion
+// @Failure 400 the request contains incorrect syntax
+// @router /:id [put]
+	func(c *CategoriaProduccionController) Put()	{
+		idStr := c.Ctx.Input.Param(":id")
+		id, _ := strconv.Atoi(idStr)
+		v := models.CategoriaProduccion{Id: id}
+		if err := json.Unmarshal(c.Ctx.Input.RequestBody, &v); err == nil {
+			v.FechaCreacion = time_bogota.TiempoCorreccionFormato(v.FechaCreacion)
+			v.FechaModificacion = time_bogota.TiempoBogotaFormato()
+			if err := models.UpdateCategoriaProduccionById(&v); err == nil {				
+						c.Data["json"] = v
+				} else {
+					logs.Error(err)
+					//c.Data["development"] = map[string]interface{}{"Code": "000", "Body": err.Error(), "Type": "error"}
+					c.Data["system"] = err
+					c.Abort("400")
+				}
+			} else {
+				logs.Error(err)
+				//c.Data["development"] = map[string]interface{}{"Code": "000", "Body": err.Error(), "Type": "error"}
+				c.Data["system"] = err
+				c.Abort("400")
+			}
+			c.ServeJSON()
+		}
 
 // Delete ...
 // @Title Delete
-// @Description delete the SistemaTipoProduccion
+// @Description delete the CategoriaProduccion
 // @Param	id		path 	string	true		"The id you want to delete"
 // @Success 200 {string} delete success!
 // @Failure 404 not found resource
 // @router /:id [delete]
-func (c *SistemaTipoProduccionController) Delete() {
+func (c *CategoriaProduccionController) Delete() {
 	idStr := c.Ctx.Input.Param(":id")
 	id, _ := strconv.Atoi(idStr)
-	if err := models.DeleteSistemaTipoProduccion(id); err == nil {
+	if err := models.DeleteCategoriaProduccion(id); err == nil {
 		c.Data["json"] = map[string]interface{}{"Id": id}
 	} else {
 		logs.Error(err)
@@ -208,3 +205,4 @@ func (c *SistemaTipoProduccionController) Delete() {
 	}
 	c.ServeJSON()
 }
+
