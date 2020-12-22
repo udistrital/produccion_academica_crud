bc883f386342d8cc9db95714b1ea01fbc4246923
CrissUD <cfpatinoc@correo.udistrital.edu.co>
2020-12-22 08:09:57 -0500

    fix: se cambia un metadato para producciones de video y obras artisticas

---
+++ database/scripts/20201222_080245_correccion_meta_obras_video.up.sql
@@ -0,0 +1,4 @@
+UPDATE produccion_academica.metadato_subtipo_produccion SET tipo_metadato_id=34 WHERE id=181;
+UPDATE produccion_academica.metadato_subtipo_produccion SET tipo_metadato_id=34 WHERE id=196;
+UPDATE produccion_academica.metadato_subtipo_produccion SET tipo_metadato_id=34 WHERE id=210;
+UPDATE produccion_academica.metadato_subtipo_produccion SET tipo_metadato_id=34 WHERE id=225;
