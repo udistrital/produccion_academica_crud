b8a83a813eb7df993f1cfe35ce6dea7ca780f123
CamiloQui <camiloquirogam@outlook.com>
2021-02-10 08:32:46 -0500

    fix/cambio de meta datos (doi y paginas)

---
+++ database/scripts/20210210_081457_actualizar_metadatos_up.sql
@@ -0,0 +1,2 @@
+update produccion_academica.tipo_metadato set form_definition = '{"etiqueta":"input","claseGrid":"col-6","nombre":"12","label_i18n":"DOI","placeholder_i18n":"DOI","requerido":false,"tipo":"text","span":""}' where id = 12;
+update produccion_academica.tipo_metadato set form_definition = '{"etiqueta":"input","claseGrid":"col-6","nombre":"1","label_i18n":"paginas","placeholder_i18n":"paginas","requerido":true,"tipo":"text","span":""}' where id = 1;
